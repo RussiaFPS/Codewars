d7b1b3c5cf3674a58fa0e6f73ba10e537fe3be37
nikita_ne <nikitakrut58@gmail.com>
2021-10-12 16:28:00 +0300

    refactoring solve

--- consonantValue/solution.go
+++ consonantValue/solution.go
@@ -6,21 +6,17 @@ import (
 
 func solve(str string) int {
 	var max,now int
-
 	for _,alp:=range []string{"a", "e", "i", "o", "u"}{
 		str=strings.ReplaceAll(str,alp," ")
 	}
-
 	for _,value:=range str{
-		if value==32{
+		switch value {
+		case 32:
 			now=0
-		}else{
-			now+=int(rune(value)-96)
-		}
-		if max<now{
-			max=now
+		default:
+			now+=int(value-96)
+			if max<now{ max=now }
 		}
 	}
-
 	return max
 }
