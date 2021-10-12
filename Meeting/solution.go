397aa1406b16e8c64f82c4c5ce76ce5c34d4b401
nikita_ne <nikitakrut58@gmail.com>
2021-10-12 21:55:31 +0300

    refactoring consonantValue

--- Meeting/solution.go
+++ Meeting/solution.go
@@ -7,18 +7,12 @@ import (
 )
 
 func Meeting(s string) string {
-	var res string
 	var persons[]string
 
 	for _,value:=range strings.Split(s,";"){
 		buf:=strings.Split(value,":")
-		persons=append(persons,fmt.Sprint(strings.ToUpper(buf[1])," ",strings.ToUpper(buf[0])))
+		persons=append(persons,fmt.Sprintf("(%s, %s)",strings.ToUpper(buf[1]),strings.ToUpper(buf[0])))
 	}
 	sort.Strings(persons)
-	for _,value:=range persons{
-		buf:=strings.Split(value," ")
-		res+=fmt.Sprintf("(%s, %s)",buf[0],buf[1])
-	}
-
-	return res
+	return strings.Join(persons,"")
 }
